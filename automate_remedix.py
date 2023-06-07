cf3a0593dcbe451f3ed1d9c2ab33033b8733ffc2
Pessy <pessy.hollander@gmail.com>
2023-06-07 22:20:24 +0200

    deb

--- automate_remedix.py
+++ automate_remedix.py
@@ -380,6 +380,7 @@ def upload_pod_to_remedix(sftp):
             with os.scandir(upload_pod_from_server_dir) as entries:
                 for entry in entries:
                     if (f'{today}.pdf') in entry.name:
+                        print("entry.name")
                         sftp.put(f'{upload_pod_from_server_dir}{entry.name}', '/From\ Cibeez/.' )
                         msg = f'Uploaded pod file {entry.name} to remedix for today: {today}'
                         print(msg)
